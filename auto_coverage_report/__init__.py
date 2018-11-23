3fe7543994485892647f4a764802e5440430f3d9
Embrace <silviolleite@gmail.com>
2018-11-23 09:30:59 -0200

    Including the coverage options to runtests command

--- auto_coverage_report/__init__.py
+++ auto_coverage_report/__init__.py
@@ -7,20 +7,13 @@ sys.path.append(os.path.dirname(os.getcwd()))
 
 def main(argv):
     try:
-        opts, args = getopt.getopt(argv, "source:", ['source='])
+        opts, args = getopt.getopt(argv, "source:include:omit:", ['source=', 'include=', 'omit='])
     except getopt.GetoptError as err:
         # print help information and exit:
         print(err)  # will print something like "option -a not recognized"
         sys.exit(2)
-    comamand = []
-    for opt, arg in opts:
-        if opt in "--source":
-            comamand.append((opt, arg))
-        else:
-            assert False, "unhandled option"
-    print(comamand)
-    # options = '='.join([op, arg for op, arg in comamand])
-    os.system('coverage run -m unittest discover')
+    command = ['='.join([opt, arg]) for opt, arg in opts]
+    os.system('coverage run {} -m unittest discover'.format(' '.join(command)))
     os.system('coverage report -m')
     sys.exit(0)
 
